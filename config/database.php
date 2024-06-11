5716fc0cfd148f74eafb601ff52a7fd4e149153b
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2024-06-11 11:34:30 +0700

    add second db configuration

--- config/database.php
+++ config/database.php
@@ -109,6 +109,25 @@ return [
             // 'trust_server_certificate' => env('DB_TRUST_SERVER_CERTIFICATE', 'false'),
         ],
 
+        'second_db' => [
+            'driver' => 'mysql',
+            'host' => env('SECOND_DB_HOST', 'localhost'),
+            'port' => env('SECOND_DB_PORT', '3306'),
+            'database' => env('SECOND_DB_DATABASE', 'laravel'),
+            'username' => env('SECOND_DB_USERNAME', 'root'),
+            'password' => env('SECOND_DB_PASSWORD', ''),
+            'unix_socket' => env('SECOND_DB_SOCKET', ''),
+            'charset' => env('SECOND_DB_CHARSET', 'utf8mb4'),
+            'collation' => env('SECOND_DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
     ],
 
     /*
@@ -144,7 +163,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
