2f9f33957b77ca0173443b6c228317af4b95002e
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2024-06-12 11:09:33 +0700

    add new api summary/sbu

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\RingController;
+use App\Http\Controllers\TrendController;
 use App\Http\Controllers\UtilisationController;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
@@ -14,25 +16,20 @@ Route::prefix('/utilisation')->group(function () {
 Route::prefix('/ring')->group(function () {
     Route::get('/{sbu}', [RingController::class, 'ring'])->name('ring');
 });
-// Route::get('/utilisation', [UtilisationController::class, 'utilisation']);
+
 
 // myApi
+Route::prefix('/api')->group(function () {
+    Route::get('/trend/{sbu}', [ApiController::class, 'ringTrend']);
+    Route::get('/summary/{sbu}/{month}', [ApiController::class, 'listOfMaxTrafficEachRing']);
+});
+// Route::get('/utilisation', [UtilisationController::class, 'utilisation']);
+
 // Route::get('/ring/{sbu}', [ApiController::class, 'listOfMaxTrafficEachRing']);
 // Route::get('/max', [ApiController::class, 'listOfMaxTrafficEachSourceToDestination']);
 // Route::get('/list/{origin}/{terminating}/{start}/{end}', [ApiController::class, 'listTraffic']);
 
-// mytestroute
-Route::get('/tes', function () {
-    $path = public_path('data utilisasi.xlsx');
-    $reader = new Xlsx();
-    $spreadsheet = $reader->load($path);
-    $sheet = $spreadsheet->getSheetByName('01. SBU_Apr24_ok');
-    $totalRows = $sheet->getHighestRow();
 
-    for ($row = 4; $row <= $totalRows; $row++) {
-        var_dump($sheet->getCell("E{$row}")->getValue());
-        var_dump($sheet->getCell("F{$row}")->getValue());
-        var_dump($sheet->getCell("H{$row}")->getValue());
-        var_dump($sheet->getCell("I{$row}")->getValue());
-    }
-});
+// mytestroute
+Route::get('/tes/{sbu}', [ApiController::class, 'listOfMaxTrafficEachRing']);
+Route::get('/createtrend/{sbu}', [TrendController::class, 'index']);
