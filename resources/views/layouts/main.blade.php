68ed67f27d02e436baca74a90e4e537149a93e6e
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2024-06-19 14:43:29 +0700

    update dynamyc asses

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,12 +8,12 @@
 
     <!-- Favicon -->
     {{-- {{ URL::asset('images/tutwuri.ico') }} --}}
-    <link rel="shortcut icon" href="../assets/images/favicon.ico" />
-    <link rel="stylesheet" href="../assets/css/backend-plugin.min.css">
-    <link rel="stylesheet" href="../assets/css/backend.css?v=1.0.0">
-    <link rel="stylesheet" href="../assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="../assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css">
-    <link rel="stylesheet" href="../assets/vendor/remixicon/fonts/remixicon.css">
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}" />
+    <link rel="stylesheet" href="{{ asset('assets/css/backend-plugin.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/backend.css?v=1.0.0') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/@fortawesome/fontawesome-free/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/line-awesome/dist/line-awesome/css/line-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/remixicon/fonts/remixicon.css') }}">
 </head>
 
 <body class="  ">
@@ -62,19 +62,19 @@
         </div>
     </footer>
     <!-- Backend Bundle JavaScript -->
-    <script src="../assets/js/backend-bundle.min.js"></script>
+    <script src="{{ asset('assets/js/backend-bundle.min.js') }}"></script>
 
     <!-- Table Treeview JavaScript -->
-    <script src="../assets/js/table-treeview.js"></script>
+    <script src="{{ asset('assets/js/table-treeview.js') }}"></script>
 
     <!-- Chart Custom JavaScript -->
-    <script src="../assets/js/customizer.js"></script>
+    <script src="{{ asset('assets/js/customizer.js') }}"></script>
 
     <!-- Chart Custom JavaScript -->
-    <script async src="../assets/js/chart-custom.js"></script>
+    <script async src="{{ asset('assets/js/chart-custom.js') }}"></script>
 
     <!-- app JavaScript -->
-    <script src="../assets/js/app.js"></script>
+    <script src="{{ asset('assets/js/app.js') }}"></script>
 
     @yield('script')
 </body>
