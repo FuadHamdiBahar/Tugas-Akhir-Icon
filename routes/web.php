cda3dcd10cbf6aa2a61303a7b28a00c3393f6f22
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2024-06-20 10:47:17 +0700

    i am back again

--- routes/web.php
+++ routes/web.php
@@ -2,27 +2,35 @@
 
 use App\Http\Controllers\ApiController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DeviceController;
 use App\Http\Controllers\RingController;
 use App\Http\Controllers\TrendController;
 use App\Http\Controllers\UtilisationController;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
-use PhpOffice\PhpSpreadsheet\Reader\Xlsx;
 
 Route::get('/', [DashboardController::class, 'dashboard']);
-Route::prefix('/utilisation')->group(function () {
-    Route::get('/{sbu}', [UtilisationController::class, 'utilisation'])->name('utilisation');
-});
-Route::prefix('/ring')->group(function () {
-    Route::get('/{sbu}', [RingController::class, 'ring'])->name('ring');
-});
+
+Route::get('/utilisation/{sbu}', [UtilisationController::class, 'utilisation'])->name('utilisation');
+
+Route::get('/{sbu}/ring/{ring}', [UtilisationController::class, 'ringUtilisation'])->name('ringUtilisation');
+
+Route::get('/device/{origin}/{terminating}', [DeviceController::class, 'index'])->name('device');
+
+Route::get('/ring/{sbu}', [RingController::class, 'ring'])->name('ring');
 
 
 // myApi
 Route::prefix('/api')->group(function () {
     Route::get('/trend/{sbu}', [ApiController::class, 'ringTrend']);
     Route::get('/summary/{sbu}/{month}', [ApiController::class, 'listOfMaxTrafficEachRing']);
+    Route::get('/link/{sbu}', [ApiController::class, 'ringLink']);
+    Route::get('/trendmonth/{origin}/{terminating}', [ApiController::class, 'listTrafficMonth']);
+    Route::get('/trendweek/{origin}/{terminating}', [ApiController::class, 'listTrafficWeek']);
 });
+
+// run it when it needs
+Route::get('/createtrend/{sbu}', [TrendController::class, 'index']);
+Route::get('/updatetrend/{sbu}', [TrendController::class, 'update']);
 // Route::get('/utilisation', [UtilisationController::class, 'utilisation']);
 
 // Route::get('/ring/{sbu}', [ApiController::class, 'listOfMaxTrafficEachRing']);
@@ -32,4 +40,3 @@ Route::prefix('/api')->group(function () {
 
 // mytestroute
 Route::get('/tes/{sbu}', [ApiController::class, 'listOfMaxTrafficEachRing']);
-Route::get('/createtrend/{sbu}', [TrendController::class, 'index']);
