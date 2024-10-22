4c4a534a6f152f43516350f83bc0b7b292ffde95
Fuad Hamdi Bahar <fuad.bahar@iconpln.co.id>
2024-10-22 10:13:55 +0700

    Point

---
+++ database/migrations/2024_10_22_101139_create_points_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('points', function (Blueprint $table) {
+            $table->id('pointid');
+            $table->string('refid');
+            $table->double('lat');
+            $table->double('lng');
+
+            // default field
+            $table->string('created_by')->default('admin@admin.com');
+            $table->string('updated_by')->default('admin@admin.com');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->useCurrent()->useCurrentOnUpdate();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('points');
+    }
+};
