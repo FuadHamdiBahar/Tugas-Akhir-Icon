fae6b9327e1d95002fc2e8aeaf96862c49c03dc8
icon fuad <icon@iconpln.com>
2025-07-11 09:37:48 +0700

    test update user

--- pycode/script.py
+++ pycode/script.py
@@ -2,7 +2,7 @@ import pandas as pd
 import mysql.connector
 import uuid
 
-USER = 'fuad'
+USER = 'fuadh'
 PASSWORD = 'plnm0b1le'
 
 raw = pd.read_excel('POP.xlsx')[['pop_id', 'pop_name', 'pop_type', 'lat', 'lng', 'SBU_Name']]
